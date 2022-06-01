7b94eff048119c782f9dcf36ad06cc7d9bd44595
Shadab-Raza <shadab.razamd@gmail.com>
2022-06-02 00:23:38 +0530

    added assignment2

---
+++ src/util/helper.js
@@ -0,0 +1,17 @@
+// const printDate = function(){
+//     const date = new Date()
+//     console.log(date.getMonth())
+// }
+// module.exports.printDate = printDate
+
+
+// const printMonth = function(){
+//     console.log("June")
+// }
+// module.exports.printMonth = printMonth
+
+
+// const getBatchInfo = function(){
+//     console.log("Redon, W03D03, the topic for today is nodejs module system")
+// }
+// module.exports.getBatchInfo = getBatchInfo
---
+++ src/validator/formatter.js
@@ -0,0 +1,22 @@
+ const trim = function(){
+    const name = ("         Md Shadab Raza             ")
+    let result = name.trim()
+    console.log(result)
+}
+module.exports.trim = trim
+
+
+const changeToLowerCase = function(){
+    const name = (" MD SHADAB RAZA ")
+    let result = name.toLowerCase()
+    console.log(result)
+}
+module.exports.changeToLowerCase = changeToLowerCase
+
+
+const changeToUpperCase = function(){
+    const name = (" MD SHADAB RAZA ")
+    let result = name.toUpperCase()``
+    console.log(result)
+}
+module.exports.changeToUpperCase = changeToUpperCase
